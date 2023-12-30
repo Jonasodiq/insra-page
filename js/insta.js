17b1c51a8dcaafa44dc26c5711758f4246ddd1e2
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2023-12-30 23:11:00 +0100

    Add audio

--- js/insta.js
+++ js/insta.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){ 
+$(document).ready(function() { 
 
+    // let userFeed = new instaFeed ({
+    //     get: 'user',
+    //     userId: '765866208759737',
+    //     accessToken: '765866208759737.8c7b437d1932b3dad230687868dd0155'
+    // });
 
+    // userFeed.run();
 
 });
